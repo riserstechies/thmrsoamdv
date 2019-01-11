b2e993252968631a236df8664add5745bfe60cb0
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-11 23:44:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Thermos-Stainless-Beverage-Bottle-Midnight/dp/B0017IHRNM/ref=sr_1_48?srs=18505439011&ie=UTF8&qid=1547230308&sr=8-48">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/Thermos-Stainless-Beverage-Bottle-Midnight/dp/B0017IHRNM/ref=sr_1_48?srs=18505439011&ie=UTF8&qid=1547230308&sr=8-48"
         </script>
         <title>Page Redirection</title>
     </head>
